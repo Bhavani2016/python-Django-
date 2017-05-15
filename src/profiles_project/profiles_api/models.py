13ee6505f22dc2913e54d715cd1e697a6f158da3
bhavani govindaraj <Bhavani2016>
2017-05-15 11:19:45 -0400

    Enabled the Django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -2,34 +2,35 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-# Create your models here.
+
+
 class UserProfileManager(BaseUserManager):
-    """Helps Django work with our custom user model"""
+    """Helps Django work with our custom user model."""
 
-def create_user(self, email, name, password=None):
-    """Create a new user profile."""
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile."""
 
-    if not email:
-        raise ValueError('Users must have an email address.')
+        if not email:
+            raise ValueError('Users must have an email address.')
 
-    email=self.normalize_email(email)
-    user=self.model(email=email,name=name,)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
-    user.set_password(password)
-    user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-    return user
+        return user
 
-def create_superuser(self, email, name, password):
-    """Creates and saves a new superuser with given details."""
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
 
-    user=self.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
-    user.is_superuser = True
-    user.is_staff = True
-    user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-    return user
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Represents a "uesr profile inside our system"""
@@ -54,6 +55,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
         return self.name
 
-    def _str_(self):
-        """Django uses this when it needs to convert the object to a string"""
-        return self.email
+        def _str_(self):
+            """Django uses this when it needs to convert the object to a string"""
+
+            return self.email
